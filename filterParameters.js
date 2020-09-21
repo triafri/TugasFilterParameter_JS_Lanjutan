604ba71eff9b7a98662b848ffb3663694d978780
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-22 00:49:00 +0700

    Add files via upload

---
+++ filterParameters.js
@@ -0,0 +1,23 @@
+function panggilFilterParameters(value){
+	var arr = [
+	{negara: 'Indonesia', benua: 'Asia'},
+	{negara: 'Jerman', benua: 'Eropa'},
+	{negara: 'Spanyol', benua: 'Eropa'},
+	{negara: 'Korea', benua: 'Asia'},
+	{negara: 'Portugal', benua: 'Eropa'},
+	{negara: 'Amerika Serikat', benua: 'Amerika'},
+ ];
+
+ var benuaAsia = arr.filter(function(item){
+ 	return item.benua === "Asia";
+	});
+
+ var benuaEropa = arr.filter(function(item){
+ 	return item.benua === "Eropa";
+	});
+
+console.log(benuaAsia);
+console.log(benuaEropa);
+}
+
+panggilFilterParameters();
